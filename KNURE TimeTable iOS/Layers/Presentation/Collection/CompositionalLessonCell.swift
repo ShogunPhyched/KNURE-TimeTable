85b0911fef0dbd674d8324fd91c1c33211c9688a
Vladislav Chapaev <chapaev.vlad@gmail.com>
2025-01-09 00:18:05 +0300

    enhanced lesson cell

--- KNURE TimeTable iOS/Layers/Presentation/Collection/CompositionalLessonCell.swift
+++ KNURE TimeTable iOS/Layers/Presentation/Collection/CompositionalLessonCell.swift
@@ -52,6 +52,7 @@ final class CompositionalLessonCell: UICollectionViewCell {
 
 		private let title: UILabel
 		private let subtitle: UILabel
+		private let decorationView: UIView
 
 		override init(frame: CGRect) {
 			title = .init(frame: .zero)
@@ -62,26 +63,55 @@ final class CompositionalLessonCell: UICollectionViewCell {
 			subtitle.adjustsFontForContentSizeCategory = true
 			subtitle.font = .preferredFont(forTextStyle: .subheadline)
 
+			decorationView = UIView()
+			decorationView.layer.cornerRadius = 2
+
 			super.init(frame: frame)
+
+			layer.cornerRadius = 4
 
 			let stackView = UIStackView(arrangedSubviews: [title, subtitle, UIView()])
 			stackView.axis = .vertical
 			stackView.spacing = 4
 
-			addSubview(stackView)
-			stackView.translatesAutoresizingMaskIntoConstraints = false
+			let rootStackView = UIStackView(arrangedSubviews: [decorationView, stackView])
+			rootStackView.axis = .horizontal
+			rootStackView.spacing = 4
+
+			addSubview(rootStackView)
+			rootStackView.translatesAutoresizingMaskIntoConstraints = false
 
 			NSLayoutConstraint.activate([
-				stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 4),
-				stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -4),
-				stackView.topAnchor.constraint(equalTo: topAnchor, constant: 4),
-				stackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -4)
+				decorationView.widthAnchor.constraint(equalToConstant: 4),
+
+				rootStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 4),
+				rootStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -4),
+				rootStackView.topAnchor.constraint(equalTo: topAnchor, constant: 4),
+				rootStackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -4)
 			])
 		}
 
 		@available(*, unavailable)
 		required init?(coder: NSCoder) {
 			fatalError("init(coder:) has not been implemented")
+		}
+
+		override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+			UIView.animate(withDuration: 0.5) {
+				self.backgroundColor = self.backgroundColor?.withAlphaComponent(1.0)
+			}
+		}
+
+		override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+			UIView.animate(withDuration: 0.5) {
+				self.backgroundColor = self.backgroundColor?.withAlphaComponent(0.35)
+			}
+		}
+
+		override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+			UIView.animate(withDuration: 0.5) {
+				self.backgroundColor = self.backgroundColor?.withAlphaComponent(0.35)
+			}
 		}
 
 		func configure(with model: Model) {
@@ -91,27 +121,35 @@ final class CompositionalLessonCell: UICollectionViewCell {
 			switch model.baseIdentifier {
 				case 0...2:
 					backgroundColor = .systemYellow.withAlphaComponent(0.35)
+					decorationView.backgroundColor = .systemYellow
 
 				case 10...12:
 					backgroundColor = .systemGreen.withAlphaComponent(0.35)
+					decorationView.backgroundColor = .systemGreen
 
 				case 20...24:
 					backgroundColor = .systemPurple.withAlphaComponent(0.35)
+					decorationView.backgroundColor = .systemPurple
 
 				case 40...41:
 					backgroundColor = .systemPink.withAlphaComponent(0.35)
+					decorationView.backgroundColor = .systemPink
 
 				case 50...55:
 					backgroundColor = .systemBlue.withAlphaComponent(0.35)
+					decorationView.backgroundColor = .systemBlue
 
 				case 60...65:
 					backgroundColor = .systemPink.withAlphaComponent(0.35)
+					decorationView.backgroundColor = .systemPink
 
 				case 1337:
 					backgroundColor = .clear
+					decorationView.backgroundColor = .clear
 
 				default:
 					backgroundColor = .systemGray.withAlphaComponent(0.35)
+					decorationView.backgroundColor = .systemGray
 			}
 		}
 	}
